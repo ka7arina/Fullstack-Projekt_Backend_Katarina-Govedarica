90f328e26071737bee5d455d90b4957e160a0dc8
KatarinaGovedarica <145567220+ka7arina@users.noreply.github.com>
2024-08-15 15:21:31 +0200

    created the user files, struggling with the security

--- vinyl-store/src/main/java/noseryoung/ch/vinyl_store/User/UserEntity.java
+++ vinyl-store/src/main/java/noseryoung/ch/vinyl_store/User/UserEntity.java
@@ -46,4 +46,88 @@ public class UserEntity {
     @ManyToOne
     @JoinColumn(name = "ID_role",referencedColumnName = "role_ID")
     private UserRolesEntity user_has_role;
+
+    public String getUsername() {
+        return email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(int postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public UserRolesEntity getUser_has_role() {
+        return user_has_role;
+    }
+
+    public void setUser_has_role(UserRolesEntity user_has_role) {
+        this.user_has_role = user_has_role;
+    }
 }
--- vinyl-store/src/main/java/noseryoung/ch/vinyl_store/User/UserRolesEntity.java
+++ vinyl-store/src/main/java/noseryoung/ch/vinyl_store/User/UserRolesEntity.java
@@ -26,4 +26,28 @@ public class UserRolesEntity {
 
     @OneToMany(mappedBy = "user_has_role")
     private List<UserEntity> role_assigned_to_users = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<UserEntity> getRole_assigned_to_users() {
+        return role_assigned_to_users;
+    }
+
+    public void setRole_assigned_to_users(List<UserEntity> role_assigned_to_users) {
+        this.role_assigned_to_users = role_assigned_to_users;
+    }
 }
